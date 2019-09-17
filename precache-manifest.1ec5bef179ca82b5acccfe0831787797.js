a5aa598f57d29bd6e0f751330a2fcf8193ca216a
tere <tere@gmail.com>
2019-09-17 00:27:43 -0600

    Updates

---
+++ precache-manifest.1ec5bef179ca82b5acccfe0831787797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51457f624cd30b7016ad6aad921bd2e1",
+    "url": "/RetoLogin/index.html"
+  },
+  {
+    "revision": "b8cef8b4ddc14b640e50",
+    "url": "/RetoLogin/static/css/main.a21117b1.chunk.css"
+  },
+  {
+    "revision": "f86125531bbdb8f3214c",
+    "url": "/RetoLogin/static/js/2.b26a188f.chunk.js"
+  },
+  {
+    "revision": "b8cef8b4ddc14b640e50",
+    "url": "/RetoLogin/static/js/main.1f7132e4.chunk.js"
+  },
+  {
+    "revision": "7de4270c8ce0290e4f15",
+    "url": "/RetoLogin/static/js/runtime~main.62bf5b05.js"
+  }
+]);
